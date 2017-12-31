2043e88d283eb5590f4f7071d527d200e63cc066
Mathias Kluba <mathias.kluba@gmail.com>
2017-12-31 14:12:14 +0100

    add GELF support for solr logging

--- solrcloud/entrypoint2.sh
+++ solrcloud/entrypoint2.sh
@@ -1,3 +1,15 @@
-set -x
+# the data folder, need chown after docker volume mount
 chown -R solr /solr/example/cloud
+
+# enable Graylog2
+if [[ $GRAYLOG2_HOST ]]; then
+  CURRENT_GRAYLOG2_PORT="${GRAYLOG2_PORT:-12201}"
+  echo "Graylog2 enabled to $GRAYLOG2_HOST:$CURRENT_GRAYLOG2_PORT"
+  sed -i -e "s/log4j.appender.gelf.Host=udp:localhost/log4j.appender.gelf.Host=udp:$GRAYLOG2_HOST/g" /tmp/log4j.properties
+  sed -i -e "s/log4j.appender.gelf.Port=12201/log4j.appender.gelf.Port=$CURRENT_GRAYLOG2_PORT/g" /tmp/log4j.properties
+  cat /tmp/log4j.properties >> /solr/example/resources/log4j.properties
+  echo "log4j configuration:"
+  cat /solr/example/resources/log4j.properties
+fi
+
 /entrypoint.sh
