a0ff7ae8b13a55d4b58e34d0210f1d9f33d33b73
Mathias Kluba <mathias.kluba@gmail.com>
2017-12-31 14:43:57 +0100

    fetch rancher information in GELF logs

--- solrcloud/entrypoint.sh
+++ solrcloud/entrypoint.sh
@@ -16,6 +16,18 @@ if [[ $GRAYLOG2_HOST ]]; then
   echo "Graylog2 enabled to $GRAYLOG2_HOST:$CURRENT_GRAYLOG2_PORT"
   sed -i -e "s/log4j.appender.gelf.Host=udp:localhost/log4j.appender.gelf.Host=udp:$GRAYLOG2_HOST/g" /tmp/log4j.properties
   sed -i -e "s/log4j.appender.gelf.Port=12201/log4j.appender.gelf.Port=$CURRENT_GRAYLOG2_PORT/g" /tmp/log4j.properties
+
+  # if in rancher, retrieve container data
+  if curl --output /dev/null --silent --head --fail "http://rancher-metadata/latest/version"; then
+    echo "Container launched in Rancher, fetching additional data"
+    RANCHER_CONTAINER_NAME=$(curl -s http://rancher-metadata/latest/self/container/name)
+    RANCHER_SERVICE_NAME=$(curl -s http://rancher-metadata/latest/self/service/name)
+    RANCHER_STACK_NAME=$(curl -s http://rancher-metadata/latest/self/stack/name)
+
+    sed -i -e "s/log4j.appender.gelf.AdditionalFields=applicationName=solr/log4j.appender.gelf.AdditionalFields=applicationName=solr,rancherContainerName=$RANCHER_CONTAINER_NAME,rancherServiceName=$RANCHER_SERVICE_NAME,rancherStackName=$RANCHER_STACK_NAME/g" /tmp/log4j.properties
+    sed -i -e "s/log4j.appender.gelf.AdditionalFieldTypes=applicationName=String/log4j.appender.gelf.AdditionalFieldTypes=applicationName=String,rancherContainerName=String,rancherServiceName=String,rancherStackName=String/g" /tmp/log4j.properties   
+  fi
+
   cat /tmp/log4j.properties >> /solr/example/resources/log4j.properties
   echo "log4j configuration:"
   cat /solr/example/resources/log4j.properties
