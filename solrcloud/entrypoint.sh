cf6cf376bd4081237306c13ef89de1b879034872
Mathias Kluba <mathias.kluba@gmail.com>
2017-12-31 14:27:00 +0100

    switch from alpine to ubuntu

--- solrcloud/entrypoint.sh
+++ solrcloud/entrypoint.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export SOLR_USER="solr"
+
 # the data folder, need chown after docker volume mount
 chown -R solr /solr/example/cloud
 
@@ -12,4 +21,16 @@ if [[ $GRAYLOG2_HOST ]]; then
   cat /solr/example/resources/log4j.properties
 fi
 
-/entrypoint.sh
+if [ $# -gt 0 ]; then
+    exec $@
+else
+    if [ "$(whoami)" = "root" ]; then
+        su - "$SOLR_USER" <<-EOF
+            # preserve PATH from root
+            export PATH="$PATH"
+            /solr-start.sh
+EOF
+    else
+        /solr-start.sh
+    fi
+fi
